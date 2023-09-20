fb9a420006ff199732fcf68111490fa3b6f3fa38
DuCLeK65t <llduc.312@gmail.com>
2023-09-20 18:30:43 +0700

    Add IR Device with Arduino

---
+++ _Others/Read-IR-Signal/Read-IR-Signal.ino
@@ -0,0 +1,22 @@
+#include <IRremote.h>
+ 
+int RECV_PIN = 8; // IR LED pin
+ 
+decode_results results;
+ 
+void setup()
+{
+  Serial.begin(9600);
+  IrReceiver.begin(RECV_PIN, ENABLE_LED_FEEDBACK); // Start the receiver
+}
+ 
+void loop() {
+  if (IrReceiver.decode()) { // Check if is there any IR Signal
+      Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX); // Print "old" raw data
+      // USE NEW 3.x FUNCTIONS
+      IrReceiver.printIRResultShort(&Serial); // Print complete received data in one line
+      IrReceiver.printIRSendUsage(&Serial);   // Print the statement required to send this data
+      
+      IrReceiver.resume(); // Enable receiving of the next value
+  }
+}
---
+++ _Others/Toggle-LED-with-IR-Remote-Control/Toggle-LED-with-IR-Remote-Control.ino
@@ -0,0 +1,28 @@
+#include <IRremote.h>
+
+int RECV_PIN = 8; // IR LED pin
+int ledPin = 10;
+
+decode_results results;
+
+void setup() {
+  Serial.begin(9600);
+  IrReceiver.begin(RECV_PIN, ENABLE_LED_FEEDBACK);  // Start the receiver
+
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  if (IrReceiver.decode()) {  // Check if is there any IR Signal
+    switch (IrReceiver.decodedIRData.decodedRawData) {
+      case 0xB847FF00:  // CH +
+        digitalWrite(ledPin, HIGH);
+        break;
+      case 0xB946FF00:  // CH -
+        digitalWrite(ledPin, LOW);
+        break;
+    }
+
+    IrReceiver.resume();  // Enable receiving of the next value
+  }
+}
